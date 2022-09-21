322c3fa4e294d6bc82d5203d3d8554a6d57709e8
Lucas Brandão <brandao.brandao@discente.ufg.br>
2022-09-21 01:25:13 -0300

    Added new methods to game and enemy

--- enemies/enemy.py
+++ enemies/enemy.py
@@ -1,20 +1,34 @@
+from turtle import width
 import pygame
 from traitlets import Bool
 
 
 class Enemy:
     imgs = []
-    def __init__(self, x, y) -> None:
+    def __init__(self, x, y, width, height) -> None:
         self.x = x
         self.y = y
+        self.width = width
+        self.height = height
         self.animation_count = 0
         self.health = 1
         self.path = []
+        self.img = None
 
     def draw(self, win: pygame.Surface) -> None:
-        pass
+        self.animation_count += 1
+        self.img = self.imgs[self.animation_count]
+
+        if self.animation_count >= len(self.imgs):
+            self.animation_count = 0
+
+        win.blit(self.img, (self.x, self.y))
+        self.move()
 
     def collide(self, x: int, y: int) -> Bool:
+        if x <= self.x + self.width and x >= self.x:
+            if y <= self.y + self.height and y >= self.y:
+                return True
         return False
 
     def move(self) -> None:
--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import pygame
+import os
 
 
 class Game:
@@ -10,13 +11,33 @@ class Game:
         self.towers = []
         self.lives = 10
         self.money = 100
+        self.bg = pygame.image.load(os.path.join("game_assets", "bg.png"))
+        self.clicks = [] # remove
 
     def run(self):
         run = True
-
+        clock = pygame.time.Clock()
         while run:
+            clock.tick(60)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     run = False
 
+                pos = pygame.mouse.get_pos()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    self.clicks.append(pos)
+                    print(pos)
+
+            self.draw()
+
         pygame.quit()
+
+    def draw(self):
+        self.win.blit(self.bg, (0,0))
+        for p in self.clicks:
+            pygame.draw.circle(self.win, (255,0,0), (p[0],p[1]), 5, 0)
+        pygame.display.update()
+
+if __name__ == "__main__":
+    game = Game()
+    g.run()
