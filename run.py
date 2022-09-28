e7c9735a6d2982b762e9975f99fb08abb043c248
Lucas Brandão <brandao.brandao@discente.ufg.br>
2022-09-28 01:14:36 -0300

    Update run.py

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 import pygame
-from main_menu.main_menu import MainMenu
 
 if __name__ == "__main__":
     pygame.init()
     win = pygame.display.set_mode((1350, 700))
+    from main_menu.main_menu import MainMenu
     mainMenu = MainMenu(win)
     mainMenu.run()
